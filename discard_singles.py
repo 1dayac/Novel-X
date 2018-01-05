3ddc6126c9713d796820913369890c68347c251b
Dima <dima@mac178124.med.cornell.edu>
2018-01-05 13:49:08 -0500

    New discard singles

--- discard_singles.py
+++ discard_singles.py
@@ -5,21 +5,29 @@ samfile_in = pysam.AlignmentFile(sys.argv[1], "rb")
 samfile_out = pysam.AlignmentFile(sys.argv[2], "wb", header=samfile_in.header)
 
 current_name = ""
-
+records = []
 for r in samfile_in:
     if current_name == "":
         current_name = r.query_name
-        prev_record = r
+        records.append(r)
         continue
     if current_name == r.query_name:
-        samfile_out.write(prev_record)
-        samfile_out.write(r)
-        current_name = ""
+        records.append(r)
         continue
     if current_name != r.query_name:
+        if len(records) != 1:
+            for r in records:
+                samfile_out.write(r)
+        records.clear()
+
         current_name = r.query_name
-        prev_record = r
+        records.append(r)
         continue
+
+if len(records) != 1:
+    for r in records:
+        samfile_out.write(r)
+records.clear()
 
 samfile_in.close()
 samfile_out.close()
