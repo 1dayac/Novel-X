4cc9c69cd38336e83a4d4e7c0999b3ae9f3d1401
Dima <dima@mac178124.med.cornell.edu>
2018-01-12 17:27:01 -0500

    Support rev_complement mappings

--- bam_to_vcf.py
+++ bam_to_vcf.py
@@ -2,6 +2,7 @@ import pysam
 #import vcf
 import sys
 import os
+from Bio.Seq import Seq
 
 def Usage():
     print("bam_to_vcf.py iterates through all sam-files in a given directory and constructs VCF files with information abou insertions")
@@ -22,13 +23,15 @@ with open(sys.argv[2], "w") as vcf:
                 cigartuples = aligned_segment.cigartuples
                 ref_start = aligned_segment.reference_start
                 read_start = 0
-
+                seq = Seq(aligned_segment.query_sequence)
+                if aligned_segment.is_reverse:
+                    seq = seq.reverse_complement()
                 for tuple in cigartuples:
                     if tuple[0] == 0:
                         ref_start += tuple[1]
                         read_start += tuple[1]
                     if tuple[0] == 1:
-                        vcf.write(ref_id + "\t" + ref_start + "\t" + "." + "\t" + aligned_segment.query_sequence[read_start] + "\t" + aligned_segment.query_sequence[read_start:read_start + tuple[1]] + "\t" + "." + "\t" + "PASS" + "\t" + "DP=100" + "\n")
+                        vcf.write(ref_id + "\t" + ref_start + "\t" + "." + "\t" + seq[read_start] + "\t" + seq[read_start:read_start + tuple[1]] + "\t" + "." + "\t" + "PASS" + "\t" + "DP=100" + "\n")
                         read_start += tuple[1]
 
                     if tuple[0] == 2:
