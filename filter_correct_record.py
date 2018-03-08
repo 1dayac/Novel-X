eef6af73100b19a582ff637c88389f82e26eaae7
Dima <dima@mac178124.med.cornell.edu>
2018-03-07 19:38:11 -0500

    filter correct record

---
+++ filter_correct_record.py
@@ -0,0 +1,12 @@
+import sys
+from Bio import SeqIO
+
+with open(sys.argv[2], "r") as nucmer_file:
+    lines = nucmer_file.readlines()
+    record_dict = SeqIO.to_dict(SeqIO.parse(sys.argv[1], "fasta"))
+
+    with open(sys.argv[3], "w") as output_handle:
+        if lines[1].startswith("CONTIG"):
+            pass
+        else:
+            SeqIO.write([record_dict[lines[1].split("\t")[0].strip()]], output_handle, "fasta")
